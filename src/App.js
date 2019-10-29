a8699d3e8c2f67219b396bc9fb82382165757d07
jaswanth <jaswanth.bellam>
2019-10-29 01:34:26 -0500

    change title and logo. Align the clocks side by side

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
         <span><Clock title="Chicago" datediff={0} /></span>
         <span><Clock title="New York" datediff={1} /></span>
         <span><Clock title="London" datediff={5} /></span>
-        <span><Clock title="Paris" datediff={6} /></span>
         <span><Clock title="Moscow" datediff={8} /></span>
         <span><Clock title="Tokyo" datediff={9} /></span>
       </div>
